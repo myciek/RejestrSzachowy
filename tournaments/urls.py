e46847ba4caabb37962cca142b3395606504810f
myciek <maciejmycielski@gmail.com>
2021-07-01 09:52:43 +0200

    Show only tournaments that you were in

--- tournaments/urls.py
+++ tournaments/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 
-from tournaments.views import create_tournament
+from tournaments.views import create_tournament, tournament_details, tournament_list
 
+app_name = "tournaments"
 urlpatterns = [
     path("create/", create_tournament, name="create_tournament"),
+    path("<pk>/", tournament_details, name="tournament_details"),
+    path("", tournament_list, name="tournament_list"),
+
 ]
--- tournaments/views.py
+++ tournaments/views.py
@@ -1,7 +1,10 @@
+from django.db.models import Q
 from django.shortcuts import render, redirect
 
 # Create your views here.
+from matches.models import Match
 from tournaments.forms import TournamentForm
+from tournaments.models import Tournament
 
 
 def create_tournament(request):
@@ -15,3 +18,27 @@ def create_tournament(request):
             form = TournamentForm(request.POST)
 
     return render(request, "tournaments/tournaments.html", {"form": form})
+
+
+def tournament_details(request, pk):
+    tournament = Tournament.objects.get(id=pk)
+    matches = Match.objects.filter(tournament=tournament.id)
+    context = {"tournament":
+                   {"name": tournament.name,
+                    "matches": matches}}
+    return render(request, "tournaments/matches_list.html", context)
+
+
+def tournament_list(request):
+    tournaments = []
+    if request.user.is_arbiter:
+        tournaments = Tournament.objects.filter(arbiter=request.user)
+    else:
+        for match in request.user.matches_white.all():
+            tournaments.append(match.tournament)
+        for match in request.user.matches_black.all():
+            tournaments.append(match.tournament)
+        tournaments = set(tournaments)
+
+    context = {"tournaments": tournaments}
+    return render(request, "tournaments/tournaments_list.html", context)
